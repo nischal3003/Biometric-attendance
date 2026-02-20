5a96bad015008b75559b677bfab5e5a265b70825
nischal3003 <147409019+nischal3003@users.noreply.github.com>
2026-02-20 12:33:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
-# version from your app
-from biometric_attendance import __version__ as version
-
 setup(
     name="biometric_attendance",
-    version=version,
+    version="0.0.1",
     description="Auto Sync for HRMS",
     author="nischal",
     author_email="nischalraj3003@gmail.com",
@@ -16,4 +13,5 @@ setup(
     zip_safe=False,
     include_package_data=True,
     install_requires=install_requires,
+
 )
